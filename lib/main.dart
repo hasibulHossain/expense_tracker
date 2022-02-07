4aa0be6a03b6c19cc5413b72dec410f529a22a56
Hasibul Hossain <hasibulhossain52@gmail.com>
2022-02-07 15:02:57 +0600

    added intl package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+// core imports
 import 'package:flutter/material.dart';
 
+// third party imports
+import 'package:intl/intl.dart';
+
+// local imports
 import './models/transection.dart';
 
 void main() => runApp(MyApp());
@@ -17,7 +22,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   final List<Transaction> transactions = [
     Transaction(id: 't1', title: 'Vehicle', amount: 23.9, time: DateTime.now()),
-    Transaction(id: 't2', title: 'Food', amount: 2.90, time: DateTime.now())
+    Transaction(id: 't2', title: 'Food', amount: 22.90, time: DateTime.now())
   ];
 
   @override
@@ -45,7 +50,8 @@ class MyHomePage extends StatelessWidget {
                     ),
                     child: Card(
                       child: Container(
-                        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 10),
                         child: Row(
                           children: <Widget>[
                             Container(
@@ -53,16 +59,37 @@ class MyHomePage extends StatelessWidget {
                               child: Container(
                                 padding: const EdgeInsets.all(10),
                                 decoration: BoxDecoration(
-                                  border: Border.all(color: Colors.pink.shade200, width: 2),
+                                  border: Border.all(
+                                      color: Colors.pink.shade200, width: 2),
                                   color: Colors.pink.shade200,
                                 ),
-                                child: Text(item.amount.toStringAsFixed(2)),
+                                child: Text(
+                                  '\$${item.amount.toStringAsFixed(2)}',
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
                               ),
                             ),
                             Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
-                                Text(item.title),
-                                Text('${item.time}'),
+                                Container(
+                                  margin: const EdgeInsets.only(bottom: 5),
+                                  child: Text(
+                                    item.title,
+                                    style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  DateFormat.yMMMd().format(item.time),
+                                  style: const TextStyle(color: Colors.grey),
+                                ),
                               ],
                             )
                           ],
